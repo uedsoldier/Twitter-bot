8fbb82e18303cb8e586c62bfb750d69cc3018756
Roberto Parra <uedsoldier1990@gmail.com>
2025-08-02 23:11:51 -0600

    Variables naming convention

--- tests/test_redis.py
+++ tests/test_redis.py
@@ -11,10 +11,10 @@ REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', '')
 
 r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, username=REDIS_USER,password=REDIS_PASSWORD,decode_responses=True)
 
-fecha_inicio_conteo_banos_str = str(r.get('fecha_inicio_conteo_banos'))
-fecha_inicio_conteo_banos = datetime.strptime(fecha_inicio_conteo_banos_str, '%Y-%m-%d')
+initial_date_str = str(r.get('initial_date'))
+initial_date = datetime.strptime(initial_date_str, '%Y-%m-%d')
 
-hoy = datetime.today()
-dias = (hoy - fecha_inicio_conteo_banos).days
+today = datetime.today()
+days_since_begin = (today - initial_date).days
 
-print(f"Han pasado {dias} días desde que comenzó el conteo de Santiago Baños.")
+print(f"Han pasado {days_since_begin} días desde que comenzó el conteo.")
