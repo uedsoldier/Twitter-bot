ea17b8e381fcba18e8f7c446bf90163f461094d4
Roberto Parra <uedsoldier1990@gmail.com>
2025-08-04 00:36:35 -0600

    Improved redis store class with error checking

--- src/twitter_bot/redis_store.py
+++ src/twitter_bot/redis_store.py
@@ -1,39 +1,75 @@
 import redis
 from twitter_bot.config import REDIS_HOST, REDIS_PORT, REDIS_USER, REDIS_PASSWORD
+from redis.exceptions import RedisError, ConnectionError, AuthenticationError, TimeoutError
 
 class RedisStore:
     def __init__(self):
-        self.r = redis.Redis(
-            host=REDIS_HOST,
-            port=REDIS_PORT,
-            username=REDIS_USER,
-            password=REDIS_PASSWORD,
-            decode_responses=True
-        )
+        try:
+            self.r = redis.Redis(
+                host=REDIS_HOST,
+                port=REDIS_PORT,
+                username=REDIS_USER,
+                password=REDIS_PASSWORD,
+                decode_responses=True
+            )
+        except RedisError as e:
+            self.r = None
     
     def get(self, key):
-        return self.r.get(key)
+        try:
+            return self.r.get(key)
+        except RedisError as e:
+            return None
+            
     
-    def set(self, key, value):
-        self.r.set(key, value)
+    def set(self, key, value) -> bool:
+        try:
+            self.r.set(key, value)
+            return True
+        except RedisError as e:
+            return False
+            
 
-    def delete(self, key):
-        self.r.delete(key)
+    def delete(self, key) -> bool:
+        try:
+            self.r.delete(key)
+            return True
+        except RedisError as e:
+            return False
 
-    def sadd(self, key, value):
-        self.r.sadd(key, value)
+    def sadd(self, key, value) -> bool:
+        try:
+            self.r.sadd(key, value)
+            return True
+        except RedisError as e:
+            return False
     
     def sismember(self, key, value):
-        return self.r.sismember(key, value)
+        try:
+            return self.r.sismember(key, value)
+        except RedisError as e:
+            return None
     
     def scard(self, key):
-        return self.r.scard(key)
+        try:
+            return self.r.scard(key)
+        except RedisError as e:
+            return None
     
     def incr(self, key):
-        return self.r.incr(key)
+        try:
+            return self.r.incr(key)
+        except RedisError as e:
+            return None
     
     def smembers(self, key):
-        return self.r.smembers(key)
+        try:
+            return self.r.smembers(key)
+        except RedisError as e:
+            return None
     
     def ping(self):
-        return self.r.ping()
+        try:
+            return self.r.ping()
+        except RedisError as e:
+            return None
